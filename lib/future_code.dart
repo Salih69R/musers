50ec2709a7ed058c20d1469b6100777957a6df3d
SalihR <salihrmah@gmail.com>
2021-08-24 15:56:04 +0300

    worked a bit on user json file

--- lib/future_code.dart
+++ lib/future_code.dart
@@ -12,6 +12,45 @@
 
 //tags system
 
+
 //db json objects:
-//user, post, comment, mention, chat, message, ...?
+  //user, other_user, band 
+
+
+class user{
+  final String email;
+  String password;
+  final String first_name;
+  final String last_name;
+  final String user_name;
+
+  String profile_image = "";
+  String about_me = "I'm a new Muser!";
+  bool remember_me = false;
+  int number_of_posts = 0;
+  int number_of_friends = 0;
+
+  List<post> posts = [];
+  List<media> medias = [];
+  List<mention> mentions = [];
+  
+  //the tag "new_muser" can be used to show more unrelated content to new users, since they don't have enough tags
+  List<String> tags = ["new_muser"];
+
+  user(this.email, this.password, this.first_name, this.last_name, this.user_name); 
+}
+  //post, media, comment, mention
+  class post{}
+
+  class media{}
+
+  class comment{}
+  
+  class mention{}
+
+
+  //chat, message, ...?
+  class chat{}
+  
+  class message{}
 

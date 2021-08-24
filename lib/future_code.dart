670c050e610c720a0dc3beffa3bb0c130490ba87
SalihR <salihrmah@gmail.com>
2021-08-24 15:27:33 +0300

    init the "thinking of future changes" file
    we can use this to think and leave out scribbles for easy team planning and future relooks

---
+++ lib/future_code.dart
@@ -0,0 +1,17 @@
+
+
+//this file is used to partially create and discuss and think about new code/logic to add
+
+//post_list_viewer widget -  this should take iterable<post> and show (according to some style) a (maybe infinite) listview
+
+//story_viewer widget
+
+//search_engine
+
+//recomendation_system - takes user and returns collection of entities to show (not just posts or users or bands...)
+
+//tags system
+
+//db json objects:
+//user, post, comment, mention, chat, message, ...?
+

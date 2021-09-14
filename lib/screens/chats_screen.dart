221cbd986727c47e93b062f07b89dd7968fd9386
SalihR <salihrmah@gmail.com>
2021-09-14 23:57:09 +0300

    make chats screen stateful and display loading symbol

--- lib/screens/chats_screen.dart
+++ lib/screens/chats_screen.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
-// TODO: implement
-class ChatsScreen extends StatelessWidget {
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChatsScreenState createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  bool _isLoading = true;
+
   @override
   Widget build(BuildContext context) {
-    
+    Widget loading_widget =
+        Scaffold(body: const Center(child: CircularProgressIndicator()));
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Chat"),
-      ),
-      body: Container()
-    
-    );
+        appBar: AppBar(
+          title: Text("Chat"),
+        ),
+        body: _isLoading
+            ? loading_widget
+            : Scrollbar(
+                child: ListView(
+                  restorationId: 'drives_list_view',
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  children: [
+                    // for (int i = 0; i < _drivers.length; i++)
+                    // CompactDriver(driver: _drivers[i]),
+                  ],
+                ),
+              ));
   }
 }
